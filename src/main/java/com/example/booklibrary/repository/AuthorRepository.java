33b3fed0280a92722c1b6331af056363f06d6211
Germán C.R. <germanux@gmail.com>
2026-07-18 13:26:33 +0200

    rem

--- src/main/java/com/example/booklibrary/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.booklibrary.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.booklibrary.entity.Author;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}

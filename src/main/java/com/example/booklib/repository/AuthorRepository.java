e09cf39930c4401c236028fd8da10df304e6f201
German Caballero <germanux@gmail.com>
2026-07-20 04:33:54 +0200

    Del proj

--- src/main/java/com/example/booklib/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.booklib.repository;
-
-import com.example.booklib.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}

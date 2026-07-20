b4aee8e71919775e202280ff7c32cf7c406a0f9b
German Caballero <germanux@gmail.com>
2026-07-20 05:26:12 +0200

    del

--- src/main/java/com/example/opencodepc64k/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.opencodepc64k.repository;
-
-import com.example.opencodepc64k.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}

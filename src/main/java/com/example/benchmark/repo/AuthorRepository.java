a87b80cb581781026cf2b78ffc9edc16a4779b94
Germán C.R. <germanux@gmail.com>
2026-07-21 18:59:28 +0200

    Revert all "Add Spring Boot JPA benchmark project with Author and Book entities"
    This reverts commit 0db06a2526fa1f1a10bbae3a9ae90c0f31c42954.

--- src/main/java/com/example/benchmark/repo/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.benchmark.repo;
-
-import com.example.benchmark.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
